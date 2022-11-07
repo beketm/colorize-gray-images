73736e9f55e6cd8eb7fc43973e987f723a54ec44
Beket Myrzanov <beket0101@gmail.com>
2022-11-07 21:43:39 +0600

    improvec model implementation

--- dataset.py
+++ dataset.py
@@ -4,39 +4,34 @@ from PIL import Image
 from torch.utils.data import Dataset
 import numpy as np
 import torch
+from torchvision.datasets import CIFAR100
 import torchvision.transforms as T
 from datasets import load_dataset, Image
+from skimage import io, color
+
 
 
 class CarvanaDataset(Dataset):
-    def __init__(self, mode="train", transform=None):
-        self.dataset = load_dataset("frgfm/imagenette", '160px', split=mode)
-        self.transform = transform
+    def __init__(self):
+        self.dataset = CIFAR100(root="./dataset", download=True)
 
     def __len__(self):
-        return len(self.dataset)
+        return 15
+        # return len(self.dataset)
 
     def __getitem__(self, index):
+        
+        rgb = io.imread(self.dataset[index][0])
+        lab = color.rgb2lab(rgb)
+        lab = np.moveaxis(lab, 2, 0)
 
-        image = self.dataset[index]['image'].convert("L")
-        mask = self.dataset[index]['image'].convert("RGB")
+        gray_scaled_image = torch.from_numpy(lab[0])
+        ab_values = torch.from_numpy(lab[1:3])
 
-        if self.transform is not None:
-            image = self.transform(image)
-            mask =  self.transform(mask)
-
-        return image, mask
+        return gray_scaled_image, ab_values
     
 def test():
-    ds = CarvanaDataset(
-        mode="validation",
-        transform=T.Compose([
-            T.PILToTensor(),
-            T.ConvertImageDtype(torch.float),
-            T.CenterCrop(160),
-            T.Normalize([0.0], [1.0])
-        ])
-    )
+    ds = CarvanaDataset()
 
     print(ds.__getitem__(5)[1].shape)
     print(ds.__getitem__(5)[0].shape)
--- model.py
+++ model.py
@@ -8,25 +8,35 @@ class Encoder(nn.Module):
     def __init__(self):
         super(Encoder, self).__init__()
         self.transformer = nn.Sequential(
+            T.ToTensor(),
             T.Resize((224, 224)),
+            T.Normalize([0.0], [1.0])
         )
         self.encoder = nn.Sequential(
             nn.Conv2d(1, 64, kernel_size=3, stride=2, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding="same"),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(128),
             nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding="same"),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(128),
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(256),
             nn.Conv2d(256, 256, kernel_size=3, stride=2, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding="same"),
-            nn.ReLU(),
-            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding="same"),
-            nn.ReLU(),
-            nn.Conv2d(512, 256, kernel_size=3, stride=1, padding="same"),
-            nn.ReLU()
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(256),
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(512),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(512),
+            nn.Conv2d(512, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(256)
         )
     
     def forward(self, x):
@@ -54,17 +64,21 @@ class Decoder(nn.Module):
         super(Decoder, self).__init__()
         
         self.decoder = nn.Sequential(
-            nn.Conv2d(256, 128, kernel_size=3, stride=1, padding="same"),
+            nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
+            nn.BatchNorm2d(128),
             torch.nn.Upsample(scale_factor=2),
-            nn.Conv2d(128, 64, kernel_size=3, stride=1, padding="same"),
-            nn.ReLU(),      
-            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding="same"),
+            nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
+            nn.BatchNorm2d(64),      
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
             torch.nn.Upsample(scale_factor=2),
-            nn.Conv2d(64, 32, kernel_size=3, stride=1, padding="same"),
+            nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.Conv2d(32, 2, kernel_size=3, stride=1, padding="same"),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(32, 2, kernel_size=3, stride=1, padding=1),
             nn.Tanh(),
             torch.nn.Upsample(scale_factor=2),
         )
@@ -78,8 +92,8 @@ class Koala(nn.Module):
         self.encoder = Encoder()
 
         self.feature_extractor = Inception()
-        self.fusion = nn.Conv2d(1256, 256, kernel_size=1, stride=1, padding="same")
-        
+        self.fusion = nn.Conv2d(1256, 256, kernel_size=1, stride=1, padding=0)
+        self.bnorm = nn.BatchNorm2d(256)
         self.decoder = Decoder()
 
     def forward(self, x):
@@ -90,13 +104,14 @@ class Koala(nn.Module):
 
         fusion_layer = torch.cat((z_layer, features), dim=1)
         x = self.fusion(fusion_layer)
+        x = self.bnorm(x)
         x = self.decoder(x)
         return x
         
 
 
 def test():
-    x = torch.rand((4, 1, 224, 224))
+    x = torch.rand((10, 1, 224, 224))
     model = Koala()
     preds = model(x)
     print(preds.shape)
